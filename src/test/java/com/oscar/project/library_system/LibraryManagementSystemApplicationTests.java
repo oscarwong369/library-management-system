01a0a255f228f53eb539277009240d9ff0abbed3
oscarwong369 <oscarwongworking@gmail.com>
2024-12-28 03:00:48 -0500

    init test

---
+++ src/test/java/com/oscar/project/library_system/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.project.library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
